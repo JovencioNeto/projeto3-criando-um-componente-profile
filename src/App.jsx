2993c37689f8192e39168fccf2fc2c19120e77ce
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2024-11-01 16:40:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Profile from "./components/Profile"
 export default function App(){
   return(
     <body>
-      <Profile img={'../../../public/perfil.jpeg'} name={'Jovêncio Neto'} bio={'Estudante de Desenvolvimento de Sistemas na EEEP Deputado Roberto Mesquita'} phone={'+ 55 (85) 992920898'} email={'jovencioneto75@gmail.com'} twitterUrl={'a'} githubUrl={'https://github.com/JovencioNeto'} linkedinUrl={'https://www.linkedin.com/in/jovêncio-neto-a872122b8/'}></Profile>
+      <Profile img={'./perfil.jpeg'} name={'Jovêncio Neto'} bio={'Estudante de Desenvolvimento de Sistemas na EEEP Deputado Roberto Mesquita'} phone={'+ 55 (85) 992920898'} email={'jovencioneto75@gmail.com'} twitterUrl={'a'} githubUrl={'https://github.com/JovencioNeto'} linkedinUrl={'https://www.linkedin.com/in/jovêncio-neto-a872122b8/'}></Profile>
     </body> 
   )
 }
